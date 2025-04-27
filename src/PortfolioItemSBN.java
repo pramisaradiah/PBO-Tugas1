24e748d71d7496654017cc789cb89bde930749a4
pramisaradiah <diahpramisara@gmail.com>
2025-04-28 07:59:55 +0800

    Menambahkan file PortfolioItemSBN.java

---
+++ src/PortfolioItemSBN.java
@@ -0,0 +1,9 @@
+public class PortfolioItemSBN {
+    public SuratBerhargaNegara sbn;
+    public double nominalInvestasi;
+
+    public PortfolioItemSBN(SuratBerhargaNegara sbn, double nominalInvestasi) {
+        this.sbn = sbn;
+        this.nominalInvestasi = nominalInvestasi;
+    }
+}
