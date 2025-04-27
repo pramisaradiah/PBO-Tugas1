800e76703fba73d2ca9aefde38e74ac9b614e363
pramisaradiah <diahpramisara@gmail.com>
2025-04-28 07:57:59 +0800

    Menambahkan file PortfolioItemSaham.java

---
+++ src/PortfolioItemSaham.java
@@ -0,0 +1,11 @@
+public class PortfolioItemSaham {
+    public Saham saham;
+    public int jumlahLembar;
+    public double totalNominalPembelian;
+
+    public PortfolioItemSaham(Saham saham, int jumlahLembar, double totalNominalPembelian) {
+        this.saham = saham;
+        this.jumlahLembar = jumlahLembar;
+        this.totalNominalPembelian = totalNominalPembelian;
+    }
+}
