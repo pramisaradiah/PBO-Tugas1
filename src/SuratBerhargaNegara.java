f9ee860f3468f0cce9ea4244aaf0614c2da70968
pramisaradiah <diahpramisara@gmail.com>
2025-04-28 07:47:21 +0800

    menambahkan file SBN.java

---
+++ src/SuratBerhargaNegara.java
@@ -0,0 +1,15 @@
+public class SuratBerhargaNegara {
+    public String nama;
+    public double bunga;
+    public int jangkaWaktu;
+    public String tanggalJatuhTempo;
+    public double kuotaNasional;
+
+    public SuratBerhargaNegara(String nama, double bunga, int jangkaWaktu, String tanggalJatuhTempo, double kuotaNasional) {
+        this.nama = nama;
+        this.bunga = bunga;
+        this.jangkaWaktu = jangkaWaktu;
+        this.tanggalJatuhTempo = tanggalJatuhTempo;
+        this.kuotaNasional = kuotaNasional;
+    }
+}
