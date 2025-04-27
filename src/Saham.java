f4dd29669440d8642cfba5b0379cd95213244dae
pramisaradiah <diahpramisara@gmail.com>
2025-04-28 07:42:44 +0800

    Menambahkan file Saham.java

---
+++ src/Saham.java
@@ -0,0 +1,11 @@
+public class Saham {
+    public String kode;
+    public String namaPerusahaan;
+    public double harga;
+
+    public Saham(String kode, String namaPerusahaan, double harga) {
+        this.kode = kode;
+        this.namaPerusahaan = namaPerusahaan;
+        this.harga = harga;
+    }
+}
